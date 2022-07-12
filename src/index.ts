128ce4b33f3c88b150dc2998e8c43f1c962aaeba
Paresh Chouhan <paresh@cumul.io>
2022-07-12 10:01:44 +0200

    Added HTML Hello world

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import '@cumul.io/cumulio-dashboard';
+const dashboardId = 'DASHBOARD_ID_HERE';
+
+const renderApp = (root: HTMLElement, auth: {
+  status: string;
+  key?: string;
+  token?: string;
+}) => {
+  root.innerHTML = `
+    <cumulio-dashboard dashboardId="${dashboardId}" authKey="${auth.key}" authToken="${auth.token}"></cumulio-dashboard>
+  `;
+}
+
+const fetchAuthKeyAndToken = async () => {
+  return fetch('http://localhost:4001/').then(response => response.json());
+}
+
+async function fetchAndRender() {
+  const auth = await fetchAuthKeyAndToken(); 
+  renderApp(
+    document.getElementById('root'),
+    auth
+  );
+} 
+
+fetchAndRender();
+
+// document.getElementById('root').innerHTML = '<cumulio-dashboard dashboardId="763177aa-9b93-4ae7-903e-3cb07dc593d8"></cumulio-dashboard>'
